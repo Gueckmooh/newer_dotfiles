8d486738117bbabf6ca755ff22f92f6f553b27f9
Gueckmooh <gueckmooh@pm.me>
2024-10-08 23:35:35 +0200

    Move doom to chezmoiexternal

--- chezmoi/install_scripts/run_once_03-install-doom.sh
+++ chezmoi/install_scripts/run_once_03-install-doom.sh
@@ -28,31 +28,31 @@ error() {
   printf "${red}!!! %s${reset}\n" "${*}" 1>&2
 }
 
-if [[ -d "$HOME/.config/emacs" ]]; then
-    log "Skipping doom emacs clone"
-else
-    log "Cloning doom emacs repository..."
-    git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs
-    if [[ "$?" == 0 ]]; then
-        success "Clone successful"
-    else
-        error "Error occured while cloning doom"
-        exit 1
-    fi
-fi
+# if [[ -d "$HOME/.config/emacs" ]]; then
+#     log "Skipping doom emacs clone"
+# else
+#     log "Cloning doom emacs repository..."
+#     git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs
+#     if [[ "$?" == 0 ]]; then
+#         success "Clone successful"
+#     else
+#         error "Error occured while cloning doom"
+#         exit 1
+#     fi
+# fi
 
-if [[ -d "$HOME/.config/doom" ]]; then
-    log "Skipping doom personnal config clone"
-else
-    log "Cloning personnal doom emacs config..."
-    git clone git@github.com:Gueckmooh/doom-config $HOME/.config/doom
-    if [[ "$?" == 0 ]]; then
-        success "Clone successful"
-    else
-        error "Error occured while cloning personnal doom config"
-        exit 1
-    fi
-fi
+# if [[ -d "$HOME/.config/doom" ]]; then
+#     log "Skipping doom personnal config clone"
+# else
+#     log "Cloning personnal doom emacs config..."
+#     git clone git@github.com:Gueckmooh/doom-config $HOME/.config/doom
+#     if [[ "$?" == 0 ]]; then
+#         success "Clone successful"
+#     else
+#         error "Error occured while cloning personnal doom config"
+#         exit 1
+#     fi
+# fi
 
 log "Running doom install..."
 $HOME/.config/emacs/bin/doom install --no-env
