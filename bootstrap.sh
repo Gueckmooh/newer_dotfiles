1feee2eaa2151db82e14badd1e30e1e436b1b32b
Gueckmooh <gueckmooh@pm.me>
2024-10-08 22:29:43 +0200

    Update bootstrap and install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,8 @@
 
 set -e
 
+DOTFILES_REPOSITORY="https://github.com/gueckmooh/newer_dotiles"
 
-
-
-# # Check for needed binaries
-# NEEDED_BINARIES="git curl xz sudo"
-# for bin in $NEEDED_BINARIES; do
-#     which "$bin" >/dev/null 2>&1
-#     if test $? -ne 0 ; then
-#         echo "Missing needed binary; make sure $NEEDED_BINARIES are installed"
-#         exit 1
-#     fi
-# done
-
-# mkdir -p "$HOME/.local/share"
-# git clone https://github.com/gueckmooh/newer_dotfiles "$HOME/.local/share/chezmoi"
-# mkdir -p /tmp/bin
-# env PATH=/tmp/bin:$PATH sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /tmp/bin init --apply
 
 
 main() {
@@ -30,7 +15,25 @@ main() {
 }
 
 check_needed_programs() {
-    echo caca
+    # Check for needed binaries
+    NEEDED_PROGRAMS="git curl xz sudo"
+    for bin in $NEEDED_PROGRAMS; do
+        which "$bin" >/dev/null 2>&1
+        if test $? -ne 0 ; then
+            echo "Missing needed program $bin; make sure $NEEDED_PROGRAMS are installed"
+            exit 1
+        fi
+    done
+}
+
+clone_dotfiles() {
+    mkdir -p "$HOME/.local/share"
+    git clone "$DOTFILES_REPOSITORY" "$HOME/.local/share/chezmoi"
+}
+
+run_chezmoi() {
+    mkdir -p /tmp/bin
+    env PATH=/tmp/bin:$PATH sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /tmp/bin init --apply
 }
 
 main "${@}"
