5ca46948e50f33d832860b45b1bc3998cd35c98f
Gueckmooh <gueckmooh@pm.me>
2024-10-09 13:08:08 +0200

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,28 +46,33 @@ main() {
 check_needed_programs() {
     log "Checking that needed programs are present.."
 
-    NEEDED_PROGRAMS=(git curl xz sudo)
-    for bin in ${NEEDED_PROGRAMS[@]}; do
+    NEEDED_PROGRAMS="git curl xz sudo"
+    for bin in $NEEDED_PROGRAMS; do
         which "$bin" >/dev/null 2>&1
         if test $? -ne 0 ; then
             error "Missing needed program $bin; make sure $NEEDED_PROGRAMS are installed"
             exit 1
         fi
     done
+
     success "Done checking programs!"
 }
 
 clone_dotfiles() {
     log "Cloning dotfiles.."
+
     mkdir -p "$HOME/.local/share"
     git clone "$DOTFILES_REPOSITORY" "$HOME/.local/share/chezmoi"
+
     success "Done cloning dotfiles!"
 }
 
 run_chezmoi() {
     log "Running chezmoi.."
+
     mkdir -p /tmp/bin
     env PATH=/tmp/bin:$PATH sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /tmp/bin init --apply
+
     success "Done running chezmoi! Enjoy using your new config!"
 }
 
