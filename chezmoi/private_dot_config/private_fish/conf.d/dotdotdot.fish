764fa5749a39934e9be9cc2c04190cc53377d35c
gueckmooh <gueckmooh@pm.me>
2024-10-09 08:44:29 +0100

    Remove dotdotdot

--- chezmoi/private_dot_config/private_fish/conf.d/dotdotdot.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-function __dotdotdot__
-    set -l length (math (string length -- $argv) - 1)
-    echo (string repeat -n $length ../)
-end
-
-abbr --add dotdotdot --regex '\.\.+' --position anywhere --function __dotdotdot__
