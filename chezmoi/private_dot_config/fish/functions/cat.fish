a7347dab873ba845d1818ec5da6a09bf6e5c3717
Gueckmooh <gueckmooh@pm.me>
2026-06-20 14:00:15 +0200

    Update config with arch

--- chezmoi/private_dot_config/fish/functions/cat.fish
+++ chezmoi/private_dot_config/fish/functions/cat.fish
@@ -1,4 +1,7 @@
 function cat --wraps=batcat --description 'alias cat=batcat'
-  command batcat $argv
-        
+  if which bat 2>&1 >/dev/null
+    command bat $argv
+  else
+    command batcat $argv
+  end
 end
